b001a3397f0a1bf0677cf565f87501ecf58ba1a6
Javier <schjavier@gmail.com>
2025-08-21 00:07:02 -0300

    fix: apply cors config in security config

--- src/main/java/com/brotes/api/config/SecurityConfig.java
+++ src/main/java/com/brotes/api/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,7 +27,7 @@ public class SecurityConfig {
                                                    JwtFilter jwtFilter,
                                                    UserDetailServiceImpl userDetailService) throws Exception{
 
-        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+        httpSecurity.cors(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/auth/**").permitAll()
